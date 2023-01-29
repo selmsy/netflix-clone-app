66d514c30365121b7565da2725db9d56a98eade6
selma <selma.om1@gmail.com>
2023-01-29 19:00:30 +0100

    added API request

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key = '3faea7609cfdc3ffd9193c7bc28975f5'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+
+}
+
+export default requests
